4dd7046bdc0469d89af63160c11d890221d463dd
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-23 22:59:10 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import InventoryStages from './components/InventoryStages'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8 bg-quetico-navy">
+      <InventoryStages />
+    </main>
+  )
+}
