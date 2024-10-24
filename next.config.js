5054ef0a6f3cb7c5b7a2a45c6ce74ccf9decc49d
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-23 23:05:56 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   images: {
     unoptimized: true,
-  }
+  },
+  distDir: '.next',
+  // Remove experimental features if any
 }
 
 module.exports = nextConfig
